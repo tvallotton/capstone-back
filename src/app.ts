7e0fd8090b7f2f3682090f959c41886a2124015b
tvallotton <tvallotton@uc.cl>
2022-09-07 20:10:12 -0400

    chore: basic setup for application

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env["PORT"] || 5000;
+const app = express();
+
+// middleware for parsing json objects
+app.use("/", express.json());
+
+
+app.get("/", (req, res) => {
+    res.json({"message": "hello world"})
+}); 
+
+app.listen(PORT, () => {
+    console.log(`visit http://localhost:${PORT}/`);
+});
