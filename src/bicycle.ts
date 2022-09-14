8bae6221055b03136a73711dbd05bfa9a1e58e5b
tvallotton <tvallotton@uc.cl>
2022-09-13 22:40:58 -0300

    feat: bicycle crud

--- src/bicycle.ts
+++ src/bicycle.ts
@@ -34,34 +34,54 @@ router.get("/", user({ staffOnly: true }), async (req, res) => {
 });
 
 
+/** 
+ * @swagger
+ * /bicycle/{id}:
+ *      get: 
+ *          description: returns the queried bicycle models.
+ *          parameters: 
+ *              - $ref: '#/components/parameters/bicycleId'
+ *              - $ref: '#/components/parameters/x-access-token'
+ *          responses:
+ *              '200':
+ *                 $ref: '#/components/responses/Bicycle'
+ *              '401': 
+ *                 $ref: '#/components/responses/Unauthorized'
+ *              '403':
+ *                 $ref: '#/components/responses/Forbidden'
+ */
+router.get("/:id", user({ staffOnly: true }), async (req, res) => {
+    const { id } = req.params;
+    try {
+        const query = await prisma.bicycle.findUnique({
+            where: { id: Number(id) }
+        });
+        res.json(query || NOT_FOUND);
+    } catch (e) {
+        res.json(NOT_FOUND);
+    }
+});
+
+
 /**
  * @swagger
  * /bicycle: 
- *      post: 
+ *      post:
+ *          parameters: 
+ *              $ref:  '#/components/parameters/x-access-token'
  *          consumes: 
  *              - application/json
- *          questBody:
+ *          requestBody:
  *            required: true
  *            content: 
  *                application/json: 
  *                    schema: 
- *                        type: object
- *                        properties: 
- *                            qrCode:
- *                                type: string
- *                            status: 
- *                                type: string
- *                            model: 
- *                                type: string
- *                            image: 
- *                                type: string
- *                        required: 
- *                            - qrCode
- *                            - status  
- *                            - model 
+ *                        $ref: '#/components/schemas/BicycleInput'
  *          responses: 
  *              '201': 
- *                  description: creates the bicycle
+ *                  $ref: '#/components/responses/Bicycle'
+ *              '400': 
+ *                  $ref: '#/components/responses/BadRequest'
  */
 router.post("/", user({ staffOnly: true }), async (req, res) => {
     let { qrCode, status, model } = req.body;
@@ -78,6 +98,76 @@ router.post("/", user({ staffOnly: true }), async (req, res) => {
 
 
 
+/**
+ * @swagger
+ * /bicycle: 
+ *      patch: 
+ *          consumes: 
+ *              - application/json
+ *          questBody:
+ *            required: true
+ *            content: 
+ *                application/json: 
+ *                    schema: 
+ *                        $ref: '#/components/schemas/BicycleInput'
+ *          responses: 
+ *              '200': 
+ *                  $ref: '#/components/responses/Bicycle'
+ *              '400': 
+ *                  $ref: '#/components/responses/BadRequest'
+ *              '404': 
+ *                  $ref: '#/components/responses/NotFound'
+ */
+router.patch("/", user({ staffOnly: true }), async (req, res) => {
+    let { id, qrCode, status, model } = req.body;
+    if (!id && !qrCode && !status && !model) {
+        return res.status(400);
+    }
+    try {
+        let updated = await prisma.bicycle.update({
+            where: { id },
+            data: {
+                qrCode, status, model
+            }
+        });
+        res.json(updated);
+    } catch (e) {
+        res.status(404).json(NOT_FOUND);
+    }
+});
 
+
+/**
+ * @swagger
+ * /bicycle/{id}: 
+ *      delete: 
+ *          parameters: 
+ *              - $ref: '#/components/parameters/bicycleId'
+ *          responses: 
+ *              '200': 
+ *                  $ref: '#/components/responses/Bicycle'
+ *              '404': 
+ *                  $ref: '#/components/responses/NotFound'
+ */
+router.delete("/:id", user({ staffOnly: true }), async (req, res) => {
+    let { id, } = req.params;
+    try {
+        const deleted = await prisma.bicycle.delete({
+            where: { id: Number(id) },
+        });
+        res.json(deleted);
+    } catch (e) {
+        res.status(404).json(NOT_FOUND);
+    }
+});
+
+
+
+
+const NOT_FOUND = {
+    "status": "error",
+    "es": "La bicicleta no pudo ser encontrada.",
+    "en": "The bicycle was not found."
+};
 
 export default router; 
