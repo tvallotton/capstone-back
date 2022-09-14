7f0937b420967d383cbffd96c42487e9455c8ad0
tvallotton <tvallotton@uc.cl>
2022-09-14 00:06:53 -0300

    feat: create submissions endpoint

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,18 @@ import dotenv from "dotenv";
 import user from "./user/routes";
 import docs from "./docs";
 import bicycle from "./bicycle";
-
+import submission from "./submission";
 dotenv.config();
 
 const PORT = process.env["PORT"] || 5000;
 const app = express();
 
+
 // middleware for parsing json objects
 app.use("/", express.json());
 app.use("/user/", user);
 app.use("/bicycle", bicycle);
+app.use("/submission", submission);
 app.use("/", docs);
 
 
--- src/submission.ts
+++ src/submission.ts
@@ -1,7 +1,8 @@
 import { PrismaClient, User } from "@prisma/client";
 import { Router } from "express";
-import { user } from "./user/middleware";
+import { user, Request } from "./user/middleware";
 import errors from "./errors";
+import { json } from "stream/consumers";
 export const router = Router();
 
 
@@ -28,9 +29,41 @@ const prisma = new PrismaClient();
  */
 router.get("/", user({ staffOnly: true }), async (req, res) => {
     const { take, skip, } = req.query;
-    const query = await prisma.bicycle.findMany({
+    const query = await prisma.submission.findMany({
         take: Number(take) || undefined,
         skip: Number(skip) || undefined,
     });
     res.json(query);
 });
+
+
+/** 
+ * @swagger
+ * /submission:
+ *      post: 
+ *          description: creates a new submission
+ *          responses:
+ *              '200':
+ *                  $ref: '#/components/responses/Submission'
+ *              '401': 
+ *                 $ref: '#/components/responses/Unauthorized'
+ */
+router.post("/", user(), async (req: Request, res) => {
+    const userId = req.user?.id as number;
+    const name = req.body.model as string;
+    let model = await prisma.bicycleModel.findUnique({ where: { name } });
+    try {
+        let created = await prisma.submission.create({
+            data: {
+                userId,
+                bicycleModelId: model?.id as number,
+            }
+        });
+        res.json(created);
+    }
+    catch (_e) {
+        res.json(errors.BAD_REQUEST);
+    }
+});
+
+export default router; 
